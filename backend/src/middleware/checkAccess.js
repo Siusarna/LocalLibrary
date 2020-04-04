0b81186176c31f31d8d982bc22c857c5b1f15631
Siusarna <42269105+Siusarna@users.noreply.github.com>
2020-04-05 01:40:56 +0300

    fix bug with checkAccess.js

--- backend/src/middleware/checkAccess.js
+++ backend/src/middleware/checkAccess.js
@@ -1,8 +1,8 @@
 const checkAccess = async (ctx, next) => {
   if (ctx.state.user.role !== 'librarian') {
-    ctx.throw(403, 'You must login as librarian for doing this');
+    return ctx.throw(403, 'You must login as librarian for doing this');
   }
-  next();
+  return next();
 };
 
 module.exports = {
