15cb38cff437102dd1786090815b4d8e3ba83f40
Vladislav Khmara <hmara.vladislav2@gmail.com>
2020-04-05 15:27:25 +0300

    add header and book search components to homepage

--- frontend/src/components/router.jsx
+++ frontend/src/components/router.jsx
@@ -3,11 +3,16 @@ import { Switch, Route } from "react-router-dom";
 
 import HomePage from '../pages/HomePage';
 import ProfilePage from '../pages/ProfilePage';
+import LoginPage from '../pages/LoginPage';
+import RegisterPage from '../pages/RegisterPage';
+
 
 const Router = () => (
   <Switch>
     <Route exact path='/'     component={HomePage}/>
     <Route path='/profile'    component={ProfilePage}/>
+    <Route path='/login'      component={LoginPage}/>
+    <Route path='/register'   component={RegisterPage}/>
   </Switch>
 );
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Header from '../components/header';
+import { Link } from 'react-router-dom';
+import BookSearch from '../components/bookSearch';
 
 const HomePage = () => (
-  <h1>Home Page</h1>
+  <>
+    <Header>
+      <Link to='/register'>
+        <div className='linkBox'>Sign Up</div>
+      </Link>
+      <Link to='/login'>
+        <div className='linkBox'>Sign In</div>
+      </Link>
+    </Header>
+    <BookSearch />
+  </>
 )
 
 export default HomePage;
