d9170e00e3ba4314fa379f512cf5f4c7a240f080
Vladislav Khmara <hmara.vladislav2@gmail.com>
2020-04-05 15:04:06 +0300

    add book search component

---
+++ frontend/src/components/bookSearch.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const BookSearch = () => (
+  <div className='BookSearch'>
+    <input className='searchInput'/>
+    <button><img src='/search.png'/></button>
+  </div>
+);
+
+export default BookSearch;
