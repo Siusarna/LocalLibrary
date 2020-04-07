c408afce73b56d7803f79851da5a02c9ad497726
Vladislav Khmara <hmara.vladislav2@gmail.com>
2020-04-08 00:33:08 +0300

    add link to home to logo

--- frontend/src/components/logo.jsx
+++ frontend/src/components/logo.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Logo = () => (
   <div className='Logo'>
-    Local Library
+    <Link to='/'>Local Library</Link>
   </div>
 );
 
