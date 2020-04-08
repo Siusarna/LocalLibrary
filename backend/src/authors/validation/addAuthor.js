345289fd84637836f930b566a7e23e81f3c3850a
Siusarna <42269105+Siusarna@users.noreply.github.com>
2020-04-08 15:14:14 +0300

    update validator for last commit

--- backend/src/authors/validation/addAuthor.js
+++ backend/src/authors/validation/addAuthor.js
@@ -1,10 +1,9 @@
-const {
-  isLength,
-} = require('validator');
+const { isLength } = require('validator');
+const { checkPhoto } = require('./checkPhoto');
 
 const addAuthor = (ctx, next) => {
   const {
-    firstName, lastName, yearOfBirthday, yearOfDeath,
+    firstName, lastName, yearOfBirthday, yearOfDeath, photo, description,
   } = ctx.request.body;
   if (!firstName || !isLength(firstName, {
     min: 2,
@@ -23,6 +22,12 @@ const addAuthor = (ctx, next) => {
   }
   if (yearOfDeath && yearOfDeath >= 2020) {
     return ctx.throw(400, 'Wrong year of death');
+  }
+  if (!photo || !checkPhoto(photo)) {
+    return ctx.throw(400, 'Wrong photo');
+  }
+  if (!description) {
+    return ctx.throw(400, 'Wrong description');
   }
   return next();
 };
