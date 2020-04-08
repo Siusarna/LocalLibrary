5b142ead4eee4b1e709730f3053269720d43f342
Vladislav Khmara <hmara.vladislav2@gmail.com>
2020-04-09 02:40:53 +0300

    add links to new pages

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,13 +3,23 @@ import { Link } from 'react-router-dom';
 import BookSearch from '../components/bookSearch';
 import Recommendations from '../components/recommendations';
 import NewsList from '../components/newsList';
+import SectionTitle from '../components/sectionTitle';
+import { useContext } from 'react';
+import AuthContext from '../context/authContext';
 
-const HomePage = () => (
-  <>
-    <BookSearch />
-    <Recommendations/>
-    <NewsList/>
-  </>
-)
+const HomePage = () => {
+  const { role } = useContext(AuthContext);
+  return (
+    <>
+      <BookSearch />
+      <Recommendations />
+      <NewsList />
+      <SectionTitle text='All Books' to='/books/all' />
+      <SectionTitle text='All Authors' to='/authors/all' />
+      {role === 'librarian' && <SectionTitle text='Add Book'      to='/books/add' />}
+      {role === 'librarian' && <SectionTitle text='Add Authors'   to='/authors/add' />}
+    </>
+  )
+}
 
 export default HomePage;
