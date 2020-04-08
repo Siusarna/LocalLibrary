fa47b07b6421a2925633681dffd2a41ffae56918
Vladislav Khmara <hmara.vladislav2@gmail.com>
2020-04-08 14:18:33 +0300

    add sign up page

--- frontend/src/components/router.jsx
+++ frontend/src/components/router.jsx
@@ -4,9 +4,9 @@ import { Switch, Route } from "react-router-dom";
 import HomePage from '../pages/HomePage';
 import ProfilePage from '../pages/ProfilePage';
 import SignInPage from '../pages/SignInPage';
-import RegisterPage from '../pages/RegisterPage';
 import BookPage from "../pages/BookPage";
 import AuthorPage from "../pages/AuthorPage";
+import SignUpPage from "../pages/SignUpPage";
 
 
 const Router = () => (
@@ -14,7 +14,7 @@ const Router = () => (
     <Route exact path='/'       component={HomePage}/>
     <Route path='/profile'      component={ProfilePage}/>
     <Route path='/sign-in'      component={SignInPage}/>
-    <Route path='/register'     component={RegisterPage}/>
+    <Route path='/register'     component={SignUpPage}/>
     <Route path='/books/:id'    component={BookPage}/>
     <Route path='/authors/:id'  component={AuthorPage}/>
   </Switch>
