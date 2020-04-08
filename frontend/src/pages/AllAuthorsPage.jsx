63680505484672b5ad17a112ee42f8fbf753ca64
Vladislav Khmara <hmara.vladislav2@gmail.com>
2020-04-09 02:41:17 +0300

    add page with all authors

---
+++ frontend/src/pages/AllAuthorsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import useFetch from '../hooks/useFetch'
+import AuthorList from '../components/authorList';
+
+const AllAuthorsPage = () => {
+  const { isLoaded, data: authors } = useFetch('https://fathomless-ravine-92681.herokuapp.com/api/authors');
+  if (!isLoaded) return true;
+  return (
+    <AuthorList authors={authors}/>
+  )
+}
+
+export default AllAuthorsPage;
