aa563c88e4e73b30a394bc3beecb6ff08bc4bbc9
Vladislav Khmara <hmara.vladislav2@gmail.com>
2020-04-09 02:33:42 +0300

    add form to add new author

---
+++ frontend/src/components/forms/addAuthorForm.jsx
@@ -0,0 +1,84 @@
+
+import React, { useState, useContext } from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import TextInput from '../textInput';
+import TextArea from '../textArea';
+import Select from '../select';
+import SectionTitle from '../sectionTitle';
+import AuthContext from '../../context/authContext';
+import { Redirect } from 'react-router-dom';
+import FileInput from '../fileInput';
+
+const AddAuthorForm = () => {
+  const [serverError, setServerError] = useState('');
+  const [success, setSuccess] = useState(false);
+  if (success) {
+    return <Redirect to='/authors/all' />
+  }
+
+  return (
+    <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        yearOfBirthday: '',
+        yearOfDeath: '',
+        description: '',
+        photo: '',
+      }}
+      validationSchema={Yup.object({
+        firstName: Yup.string()
+          .required('Required')
+          .max(20, 'Must be 20 characters or less'),
+        lastName: Yup.string()
+          .required('Required')
+          .max(20, 'Must be 20 characters or less'),
+        yearOfBirthday: Yup.string()
+          .matches(/^-?[1-9][0-9]?[0-9]?[0-9]?$/, 'Wrong year format'),
+        description: Yup.string()
+          .required('Required'),
+        yearOfDeath: Yup.string()
+          .matches(/^-?[1-9][0-9]?[0-9]?[0-9]?$/, 'Wrong year format'),
+        photo: Yup.string(),
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        fetch('https://fathomless-ravine-92681.herokuapp.com/api/authors', {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          method: 'POST',
+          credentials: 'include',
+          body: JSON.stringify(values),
+        })
+          .then((res) => {
+            if (res.status === 200) {
+              setSuccess(true);
+            }
+            return res.json();
+          })
+          .then((json) => {
+            if (!success) {
+              const error = json.message || 'Server Error';
+              setServerError(error);
+            }
+            setSubmitting(false);
+          })
+      }}
+    >
+      <Form>
+        <SectionTitle text='' />
+        <TextInput type="text" name="firstName" label="First Name" />
+        <TextInput type="text" name="lastName" label="Last Name" />
+        <TextInput type="text" name="yearOfBirthday" label="Year of Birth" />
+        <TextInput type="text" name="yearOfDeath" label="Year of Death" />
+        <TextArea type="text" name="description" label="Description" />
+        <FileInput name='photo' label='Photo' />
+        <div className='error'>{serverError}</div>
+        <button type='submit' className='dark submit' disabled={Formik.isSubmitting}>Add Author</button>
+      </Form>
+    </Formik >
+  );
+};
+
+export default AddAuthorForm;
