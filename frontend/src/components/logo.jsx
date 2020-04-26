bbc30155443a1443765752b8206970c5e2194bd6
Vladislav Khmara <hmara.vladislav2@gmail.com>
2020-04-26 12:12:31 +0300

    add home page reloading

--- frontend/src/components/logo.jsx
+++ frontend/src/components/logo.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-const Logo = () => (
-  <div className='Logo'>
-    <Link to='/'>Local Library</Link>
+const Logo = (props) => {
+  const { pathname } = useLocation();
+  return <div className='Logo'>
+    <Link to={pathname === '/' ? '/empty' : '/'}>Local Library</Link>
   </div>
-);
+};
 
 export default Logo;
