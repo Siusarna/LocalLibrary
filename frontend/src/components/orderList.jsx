93272091d0f68397fea3b1e38882f26383c325d9
Vladislav Khmara <hmara.vladislav2@gmail.com>
2020-04-28 16:59:43 +0300

    add orders data updating

--- frontend/src/components/orderList.jsx
+++ frontend/src/components/orderList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useContext } from 'react';
 import AuthContext from '../context/authContext';
 import useFetch from '../hooks/useFetch';
@@ -8,13 +8,22 @@ import OrderStatusButton from './orderStatusButton';
 
 const LOAN_PERIOD = 30; //days
 
+const pad2 = (int) => {
+  if (int / 10 < 1) return '0' + int.toString();
+  return int.toString();
+}
+
 const formatDate = (date) => {
   if (typeof date === 'string') return date;
   const year = date.getFullYear();
   const month = date.getMonth();
   const dateNum = date.getDate();
 
-  return `${dateNum}.${month}.${year}`
+  const hours = pad2(date.getHours());
+  const minutes = pad2(date.getMinutes());
+  const seconds = pad2(date.getSeconds());
+
+  return `${dateNum}.${month}.${year} ${hours}:${minutes}:${seconds}`
 }
 
 const statusType = {
@@ -42,13 +51,20 @@ const filter = (statusFilter, idFilter) => (orders) => {
 
 const OrderList = (props) => {
   const { role } = useContext(AuthContext);
-  const [statusFilter, setStatusFilter] = useState('active') //other options are active and loaned
+  const [statusFilter, setStatusFilter] = useState('Active') //other options are active and loaned
   const [idFilter, setIdFilter] = useState('');
 
-  console.dir({ statusFilter, idFilter });
-
-  const { isLoaded, data: orders } =
+  const { isLoaded, data: orders, update } =
     useFetch('https://fathomless-ravine-92681.herokuapp.com/api/orders');
+
+  const UPDATE_INTERVAL = 5000;
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (update) update();
+    }, UPDATE_INTERVAL)
+    return () => { clearInterval(timer) };
+  })
+
   if (!isLoaded) return <div></div>;
 
   const sortedOrders = orders
@@ -90,16 +106,23 @@ const OrderList = (props) => {
             <th> Created at </th>
             {role === 'customer' && <th> Return date </th>}
             <th> Status </th>
-            {role === 'librarian' && <th> Action </th>}
-            <th> Comment </th>
+            {role === 'librarian' && <th colSpan='2'> Action </th>}
+            {role === 'customer' && <th> Comment </th>}
           </tr>
         </thead>
         <tbody>
-          {(curFilter(sortedOrders)).map(order => <Order key={order.id} order={order} role={role} />)}
+          {(curFilter(sortedOrders)).map(order =>
+            <Order
+              key={order.id}
+              order={order}
+              role={role}
+              setStatus={update}
+            />
+          )}
         </tbody>
       </table>
     </div>
   )
 };
 
-  export default OrderList;
+export default OrderList;
