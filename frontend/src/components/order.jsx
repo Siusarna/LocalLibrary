04114e5c2dfe0274208a3e37fe70b3f4365a12bf
Vladislav Khmara <hmara.vladislav2@gmail.com>
2020-04-28 17:04:46 +0300

    add action buttons to order component

--- frontend/src/components/order.jsx
+++ frontend/src/components/order.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Status from './status';
+import ConfirmOrderButton from './confirmOrderButton';
+import ConfirmationCodeButton from './confirmationCodeButton';
+import FinnishOrderButton from './finishOrderButton';
 
 const Order = (props) => {
-  const { id, bookId, title, createAt, returnAt, status, comment, 
+  const { id, bookId, title, createAt, returnAt, status, comment,
     userFirstName, userLastName } = props.order;
+  const setStatus = props.setStatus;
+  //const [curStatus, setStatus] = useState(status);
   const fullName = userFirstName + ' ' + userLastName;
   return <tr>
     <td>{id}</td>
-    {fullName && <td>{fullName}</td>}
+    {userFirstName && <td>{fullName}</td>}
     <td><Link to={`/books/${bookId}`}>{title}</Link></td>
     <td>{createAt}</td>
     {returnAt && <td>{returnAt}</td>}
-    <td><Status status={status} returnAt={returnAt}/></td>
-    {props.role === 'librarian' && <td>action</td>}
-    <td>{comment}</td>
+    <td><Status status={status} returnAt={returnAt} /></td>
+    {(props.role === 'librarian' && status === 'In-progress') &&
+      <>
+        <td colSpan='1'>
+          <ConfirmOrderButton id={id} onSuccess={() => setStatus('Ready-to-take')} />
+        </td>
+        <td colSpan='1'>
+          <Link to={'/orders/reject/' + id}><div className='linkBox'>Reject</div></Link>
+        </td>
+      </>}
+    {props.role === 'librarian' && status === 'Ready-to-take' &&
+      <td colSpan='2'><ConfirmationCodeButton id={id} /></td>}
+    {props.role === 'librarian' && status === 'Loaned' &&
+      <td colSpan='2'><FinnishOrderButton id={id} onSuccess={() => setStatus('Finished')} /></td>}
+    {props.role === 'customer' && <td>{comment}</td>}
   </tr>
 };
 
