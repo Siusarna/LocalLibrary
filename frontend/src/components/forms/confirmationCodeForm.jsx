528e74d55f1835c09af86aa2e954862f59cc3b81
Vladislav Khmara <hmara.vladislav2@gmail.com>
2020-04-28 17:03:47 +0300

    handle confirmation code requesting and checking

---
+++ frontend/src/components/forms/confirmationCodeForm.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+import TextInput from '../textInput';
+import { Redirect, useParams } from 'react-router-dom';
+import SectionTitle from '../sectionTitle';
+
+const ConfirmationCodeForm = () => {
+  const { id } = useParams();
+  const [serverError, setServerError] = useState('');
+  const [success, setSuccess] = useState(false);
+
+  if (success) {
+    return <Redirect to='/work'/>
+  }
+
+  return (
+    <>
+      <Formik
+        initialValues={{ code: '' }}
+        validationSchema={Yup.object({
+          code: Yup.string()
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setServerError('');
+          fetch('https://fathomless-ravine-92681.herokuapp.com/api/orders/confirmCode', {
+            headers: { 
+              'Content-Type': 'application/json',
+            },
+            method: 'POST',
+            credentials: 'include',
+            body: JSON.stringify({
+              code: parseInt(values.code),
+              orderId: id
+            }),
+  
+          })
+            .then((res) => {
+              console.dir({ res });
+              if (res.status === 200) {
+                setSuccess(true);
+              } else {
+                setServerError('Server error');
+              }
+              setSubmitting(false);
+              return res.json();
+            }).then(json => console.dir({ json }));
+        }}
+      >
+        <Form>
+          <TextInput
+            label='Code'
+            name='code'
+            type='code'
+          />
+          <div className='error'>{serverError}</div>
+          <button type='submit' className='dark submit' disabled={Formik.isSubmitting}>Confirm</button>
+          <SectionTitle text='Cancel' to='/work'/>
+        </Form>
+      </Formik >
+    </>
+  );
+};
+
+export default ConfirmationCodeForm;
