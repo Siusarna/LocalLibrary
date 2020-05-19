8e2b7d62289df1e17fe258e54aa33ff9711151e2
Vladislav Khmara <hmara.vladislav2@gmail.com>
2020-05-20 00:05:29 +0300

    update reviews view

--- frontend/src/components/reviews/review.jsx
+++ frontend/src/components/reviews/review.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+
 import ImageTextContainer from '../layout/imageTextContainer.jsx';
 import LeftRightContainer from '../layout/leftRightContainer.jsx';
+import AuthContext from '../../context/authContext.jsx';
+import api from '../../config/api.jsx';
 
 const pad2 = (int) => {
   if (int / 10 < 1) return `0${int.toString()}`;
@@ -20,9 +24,34 @@ const formatDate = (date) => {
   return `${dateNum}.${month}.${year} ${hours}:${minutes}:${seconds}`;
 };
 
+const deleteReview = (id, setDeleted, setHaveUserReview) => () => {
+  // eslint-disable-next-line no-alert
+  const isConfirmed = window.confirm('Do you really want to delete this review ?');
+  if (isConfirmed) {
+    fetch(api.reviews(), {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'DELETE',
+      credentials: 'include',
+      body: JSON.stringify({ id }),
+    }).then((res) => {
+      if (res.status === 200) {
+        setDeleted(true);
+        setHaveUserReview(false);
+      }
+    });
+  }
+};
 
 const Review = (props) => {
-  const { review } = props;
+  const { review, setHaveUserReview } = props;
+  const { photo, role } = useContext(AuthContext);
+  const [isDeleted, setDeleted] = useState(false);
+
+  if (isDeleted) return null;
+  if (photo === review.photo) setHaveUserReview(true);
+
   const authorName = `${review.firstName} ${review.lastName}`;
   return (
     <div className='Review'>
@@ -44,6 +73,32 @@ const Review = (props) => {
         <p>{review.content}</p>
         <div className='time'>
           <LeftRightContainer
+            left={
+              <LeftRightContainer
+                left={
+                  ((role === 'librarian')
+                    || (photo === review.photo))
+                  && <button
+                    className='linkBox'
+                    onClick={deleteReview(review.id, setDeleted, setHaveUserReview)}
+                  >
+                    Delete Review
+                  </button>
+                }
+                right={photo === review.photo
+                  && <Link
+                    to={{
+                      pathname: '/reviews/update',
+                      state: { review, bookId: props.bookId },
+                    }}
+                  >
+                    <div className='linkBox center'>
+                      Update Review
+                    </div>
+                  </Link>
+                }
+              />
+            }
             right={formatDate(new Date(review.createdAt))}
           />
         </div>
--- frontend/src/components/reviews/reviewsList.jsx
+++ frontend/src/components/reviews/reviewsList.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import Review from './review.jsx';
 import useFetch from '../../hooks/useFetch.jsx';
 import api from '../../config/api.jsx';
 import SectionTitle from '../layout/sectionTitle.jsx';
+import AuthContext from '../../context/authContext.jsx';
 
 const ReviewsList = (props) => {
+  const { role } = useContext(AuthContext);
+  const [doHaveUserReview, setHaveUserReview] = useState(false);
   const { bookId } = props;
   const { data: reviews, isLoaded } = useFetch(api.reviews(bookId));
   if (!isLoaded) return null;
 
   return (
     <div className='ReviewsList'>
-      <SectionTitle text='Reviews'/>
-      {reviews.map((review, i) => <Review review={review} key={i}/>)}
+      {role !== 'unauthorized'
+      && (!doHaveUserReview)
+      && <SectionTitle className='center' text='Create review' to={`/reviews/${bookId}/add`}/>}
+      <SectionTitle className='center' text='Reviews'/>
+      {reviews.map((review, i) => <Review review={review} key={i} bookId={bookId} setHaveUserReview={setHaveUserReview}/>)}
     </div>
   );
 };
