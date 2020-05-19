da3fcbed52c808a9cca7200eb51435c33b5a2d5f
Vladislav Khmara <hmara.vladislav2@gmail.com>
2020-05-20 02:11:05 +0300

    fix display of current user rating

--- frontend/src/components/rating/RatingSelect.jsx
+++ frontend/src/components/rating/RatingSelect.jsx
@@ -18,23 +18,22 @@ const handleChange = (bookId) => ({ target: { value } }) => {
 
 const RatingSelect = ({ bookId }) => {
   const { role } = useContext(AuthContext);
-  const { data, isLoaded, status } = useFetch(api.rating(bookId));
-  console.dir({ status, data, isLoaded });
+  const { data, status } = useFetch(api.rating(bookId));
   if (role === 'unauthorized') return null;
   if (!status) return null;
 
-  const rating = data ? data.rating : null;
+  const rating = data ? data.value : null;
 
   return (
     <form className='RatingSelect'>
       <img src='/star.png' className='starIcon' alt='rating star' />
       <select onChange={handleChange(bookId)} def>
         {(!rating) && <option value='' selected={true}>-</option>}
-        <option value='1' selected={rating === '1'}>1</option>
-        <option value='2' selected={rating === '2'}>2</option>
-        <option value='3' selected={rating === '3'}>3</option>
-        <option value='4' selected={rating === '4'}>4</option>
-        <option value='5' selected={rating === '5'}>5</option>
+        <option value='1' selected={rating === 1}>1</option>
+        <option value='2' selected={rating === 2}>2</option>
+        <option value='3' selected={rating === 3}>3</option>
+        <option value='4' selected={rating === 4}>4</option>
+        <option value='5' selected={rating === 5}>5</option>
       </select>
     </form>
   );
